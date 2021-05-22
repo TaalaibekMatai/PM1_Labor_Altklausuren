b5f90505ba99125b043ea92895d76b17e7f62aa8
Mataev, Taalaibek <taalaibek.mataev@haw-hamburg.de>
2021-05-22 17:10:38 +0200

    s

---
+++ PM1_Labor/src/ServiceMix2/ServiceMix2.java
@@ -0,0 +1,31 @@
+package ServiceMix2;
+
+public interface ServiceMix2
+{
+	/**
+     * Diese Operation bekommt ein zweidimensionales Array von int-Werten, bei dem
+     * alle Zeilen die Länge 3 haben und die Anzahl der Zeilen beliebig ist,
+     * mindestens jedoch 1. Ein korrekter Parameter hat also immer drei Spalten.
+     * Beispielsweise wäre die Variable myArray der
+     * folgenden Deklaration ein gültiger aktueller Parameter für diese Operation:<br>
+     * int[][] myArray = { { 1, 2, 3 } , { 4, 5, 6 } };  // Array von Zeilen <br>
+     * Genauso gültig wäre aber auch die folgende Deklaration:<br>
+     * int[][] myArray = { { 1, 4 } , { 2, 5 } , { 3, 6 } }; // Array von Spalten <br>
+     * Die Operation soll ein zweidimensionales Array mit gleicher Spaltenanzahl,
+     * aber einer Zeile mehr liefern. Alle int-Werte des Parameter-Arrays werden
+     * in das Ergebnis übernommen, die zusätzliche Zeile soll die Summe
+     * der übergebenen Spalten enthalten.
+     * Für beide(!) obigen Beispiele soll also folgendes geliefert werden: <br>
+     *  1 2 3 <br>
+     *  4 5 6 <br>
+     *  5 7 9 <br>
+     * Wenn ein Array mit falscher Zeilenlänge übergeben wird, soll eine
+     * IllegalArgumentException geworfen werden. Ein Array mit drei Zeilen
+     * (also der Fall 3x3) soll als ein Array von Spalten aufgefasst werden.
+     * @param matrix ein Array, in dem alle Zeilen die Länge 3 haben.
+     * @throws IllegalArgumentException
+     * @return das fast gleiche Array, lediglich ergänzt um eine Zeile mit 
+     * den Summen der Spalten. 
+     */
+    public int[][] ergaenzeUmSummenzeile(int[][] matrix);
+}
---
+++ PM1_Labor/src/ServiceMix2/ServiceMix2Impl.java
@@ -0,0 +1,68 @@
+package ServiceMix2;
+
+public class ServiceMix2Impl implements ServiceMix2
+{
+	public ServiceMix2Impl()
+	{
+
+	}
+
+	public int[][] ergaenzeUmSummenzeile(int[][] matrix)
+	{
+		int[][] erg = null;
+		if (matrix == null || matrix.length == 0)
+		{
+			throw new IllegalArgumentException();
+		}
+
+		if (matrix.length != 3)
+		{
+			for (int i = 0; i < matrix.length; i++)
+			{
+				if (matrix[i].length != 3 || matrix[i] == null)
+				{
+					throw new IllegalArgumentException();
+				}
+			}
+		}
+
+		for (int i = 0; i < matrix.length; i++)
+		{
+			if (matrix[i].length != matrix[0].length || matrix[i] == null || matrix[i].length == 0)
+			{
+				throw new IllegalArgumentException();
+			}
+		}
+
+		if (matrix.length == 3)
+		{
+			erg = new int[matrix.length][matrix[0].length + 1];
+			for (int i = 0; i < matrix.length; i++)
+			{
+				for (int j = 0; j < matrix[i].length; j++)
+				{
+					erg[i][j] = matrix[i][j];
+					erg[i][matrix[0].length] += matrix[i][j];
+				}
+			}
+		} else
+		{
+			erg = new int[matrix.length + 1][matrix[0].length];
+			for (int i = 0; i < matrix.length; i++)
+			{
+				erg[i] = matrix[i];
+				for (int j = 0; j < matrix[i].length; j++)
+				{
+					erg[matrix.length][j] += matrix[i][j];
+				}
+			}
+		}
+		return erg;
+	}
+
+	public int[][] ss()
+	{
+		int[][] myArray = { { 1, 2, 3 }, { 4, 5, 6 } };
+		return myArray;
+	}
+}
